b2f466c050776a718ccfc97e9e12bbe9c888a8b3
Adam Young <adamyoungg97@gmail.com>
2023-05-03 22:01:10 +0100

    Updated tailwind config

--- packages/tailwind-config/tailwind.config.js
+++ packages/tailwind-config/tailwind.config.js
@@ -1,6 +1,5 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  darkMode: ["class"],
   content: ["src/**/*.{js,ts,jsx,tsx}", "app/**/*.{js,ts,jsx,tsx}", "../../packages/ui/src/**/*"],
   theme: {
     container: {
