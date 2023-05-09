17494cd01ef462a442c6f89d54f38cc489b99666
Adam Young <adam.young@checkout.com>
2023-05-09 16:02:35 +0100

    Fix to params of verification endpoint

--- apps/web/app/api/strava/webhook/route.ts
+++ apps/web/app/api/strava/webhook/route.ts
@@ -2,11 +2,14 @@ import { prisma } from "@/libs/prisma";
 import { NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  const body = await request.json();
+  const { searchParams } = new URL(request.url);
+
+  if (searchParams.get("hub.verify_token") !== process.env.STRAVA_VERIFY_TOKEN) {
+    return NextResponse.next();
+  }
 
   return NextResponse.json({
-    "hub.challenge": body["hub.challenge"],
-    "hub.verify_token": process.env.STRAVA_VERIFY_TOKEN ?? "",
+    "hub.challenge": searchParams.get("hub.challenge"),
   });
 }
 
