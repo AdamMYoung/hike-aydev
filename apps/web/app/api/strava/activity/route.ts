c4035ecb0c78802d9ea6e72d999f6b562d021d2c
Adam Young <adam.young@checkout.com>
2023-05-09 16:55:29 +0100

    Added missing climbed attribute

--- apps/web/app/api/strava/activity/route.ts
+++ apps/web/app/api/strava/activity/route.ts
@@ -48,6 +48,7 @@ export async function POST(request: Request) {
     data: intersectingIds.map((id) => ({
       fellId: id,
       authorId: stravaAccount.user.id,
+      climbed: true,
     })),
   });
 
