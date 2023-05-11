1bd656caa7a2bc2a2bc251ecd7e69809b0985b70
Adam Young <adam.young@checkout.com>
2023-05-11 22:28:51 +0100

    Content tweaks

--- apps/web/app/(app)/@navigation/about/page.tsx
+++ apps/web/app/(app)/@navigation/about/page.tsx
@@ -2,12 +2,12 @@ const About = async () => {
   return (
     <div className="p-4 flex flex-col gap-4">
       <p>
-        hike.aydev.uk provides a simple way to track your progress on Wainwrights and other fells across the British
-        Isles. The project is built using Next.JS, making use of React Server Components and Server Actions which are
-        currently in beta.
+        This site provides a simple way to track your progress on Wainwrights and other fells across the British Isles.
+        The project is built using Next.JS, making use of React Server Components and Server Actions which are currently
+        in beta.
       </p>
       <p>
-        Data is provided by{" "}
+        Fell data is provided by{" "}
         <a className="underline hover:no-underline" href="http://www.hills-database.co.uk/index.html">
           The Database of British and Irish Hills
         </a>
