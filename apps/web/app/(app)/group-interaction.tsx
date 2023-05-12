32f4689d4ba0f7cbfc16dd7200f72383865667a9
Adam Young <adamyoungg97@gmail.com>
2023-05-13 00:40:49 +0100

    Removed debounced hover

--- apps/web/app/(app)/group-interaction.tsx
+++ apps/web/app/(app)/group-interaction.tsx
@@ -1,22 +1,10 @@
 "use client";
 
 import { GroupInteractionContext } from "ui";
-import debounce from "lodash.debounce";
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 
 export const GroupInteraction = ({ children }: React.PropsWithChildren) => {
   const [focusedFell, setFocusedFell] = useState<number | null>(null);
 
-  const handleSetFocusedFell = useCallback(
-    debounce((fell) => {
-      setFocusedFell(fell);
-    }, 20),
-    []
-  );
-
-  return (
-    <GroupInteractionContext value={{ focusedFell, setFocusedFell: handleSetFocusedFell }}>
-      {children}
-    </GroupInteractionContext>
-  );
+  return <GroupInteractionContext value={{ focusedFell, setFocusedFell }}>{children}</GroupInteractionContext>;
 };
