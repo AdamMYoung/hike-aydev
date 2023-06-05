81d25c00fef9b0c993808aac8d4214c4300b32f1
Adam Young <adamyoungg97@gmail.com>
2023-06-05 18:17:08 +0100

    Removed unused tsconfig setup in geospatial server

--- apps/geospatial-server/tsup.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'tsup';
-
-export default defineConfig({
-  entry: ["index.ts"],
-  external: ["database"],
-  splitting: false,
-  sourcemap: true,
-  clean: true,
-});
