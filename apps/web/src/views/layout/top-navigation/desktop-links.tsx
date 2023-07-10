9bf00642df7d9984574f7f07d9d0d131c2692866
Adam Young <adamyoungg97@gmail.com>
2023-07-10 01:02:08 +0100

    Fix to nav links

--- apps/web/src/views/layout/top-navigation/desktop-links.tsx
+++ apps/web/src/views/layout/top-navigation/desktop-links.tsx
@@ -68,21 +68,23 @@ export const DesktopLinks = ({ isUserAuthenticated }: DesktopLinksProps) => {
 };
 
 const ListItem = React.forwardRef<React.ElementRef<"a">, React.ComponentPropsWithoutRef<"a">>(
-  ({ className, title, children, ...props }, ref) => {
+  ({ className, title, children, href, ...props }, ref) => {
     return (
       <li>
         <NavigationMenuLink asChild>
-          <a
-            ref={ref}
-            className={cn(
-              "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
-              className
-            )}
-            {...props}
-          >
-            <div className="text-sm font-medium leading-none">{title}</div>
-            <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
-          </a>
+          <Link href={href as string} legacyBehavior passHref>
+            <a
+              ref={ref}
+              className={cn(
+                "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+                className
+              )}
+              {...props}
+            >
+              <div className="text-sm font-medium leading-none">{title}</div>
+              <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">{children}</p>
+            </a>
+          </Link>
         </NavigationMenuLink>
       </li>
     );
